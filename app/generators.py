c87265431f84b398487534f2418375e96e2b7848
Andrey Korzhun <steelcon@bk.ru>
2024-05-21 20:03:19 +0300

    init

---
+++ app/generators.py
@@ -0,0 +1,26 @@
+import os
+import httpx
+
+from dotenv import load_dotenv
+from openai import AsyncOpenAI
+
+load_dotenv()
+
+client = AsyncOpenAI(api_key=os.getenv('OPENAITOKEN'),
+                     http_client=httpx.AsyncClient(
+                         proxies=os.getenv('PROXYURL'),
+                         transport=httpx.HTTPTransport(local_address='0.0.0.0')
+                     ))
+
+
+async def gpt_text(req):
+    completion = await client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": req,
+            }
+        ],
+        model=os.getenv('CHATGPT_MODEL'),
+    )
+    return completion
