c1f71bd131305d0ad108fbf46e2fac24c9d2e950
Andrey Korzhun <steelcon@bk.ru>
2024-05-21 22:30:06 +0300

    TYPING

--- app/handlers.py
+++ app/handlers.py
@@ -2,6 +2,7 @@ from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
+from aiogram.enums import ChatAction
 
 import app.keyboards as kb
 import app.states as st
@@ -12,7 +13,7 @@ router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message, state: FSMContext):
-    await message.answer('Добро пожаловать в $ sudo AI bot!',
+    await message.answer('Добро пожаловать!',
                          reply_markup=kb.main)
     await state.clear()
 
@@ -25,6 +26,8 @@ async def chatting(message: Message, state: FSMContext):
 
 @router.message(st.Chat.text)
 async def chatting_result(message: Message, state: FSMContext):
+    await message.bot.send_chat_action(chat_id=message.from_user.id,
+                                       action=ChatAction.TYPING)
     await state.set_state(st.Chat.process)
     response = await gpt_text(message.text)
     await message.answer(response.choices[0].message.content)
